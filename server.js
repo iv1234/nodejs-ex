3212c5aaccd46b48f5a45dae4836e509832c5735
iv1234 <39189912+iv1234@users.noreply.github.com>
2018-05-11 15:02:31 +0000

    test2

--- server.js
+++ server.js
@@ -1,91 +1,55 @@
 //  OpenShift sample Node application
-var express = require('express'),
-    app     = express(),
-    morgan  = require('morgan');
-    
-Object.assign=require('object-assign')
-
-app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
-
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
-    mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
-    mongoURLLabel = "";
-
-if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
-  var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
-      mongoPassword = process.env[mongoServiceName + '_PASSWORD']
-      mongoUser = process.env[mongoServiceName + '_USER'];
-
-  if (mongoHost && mongoPort && mongoDatabase) {
-    mongoURLLabel = mongoURL = 'mongodb://';
-    if (mongoUser && mongoPassword) {
-      mongoURL += mongoUser + ':' + mongoPassword + '@';
-    }
-    // Provide UI label that excludes user id and pw
-    mongoURLLabel += mongoHost + ':' + mongoPort + '/' + mongoDatabase;
-    mongoURL += mongoHost + ':' +  mongoPort + '/' + mongoDatabase;
-
-  }
+  ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+var serviceSocket = null;
+var p2s = {};
+var s2p = {};
+var net = require('net');
+function clear(peerSocket) {
+  p2s[s2p[peerSocket]] = undefined;
+  s2p[peerSocket] = undefined;
 }
-var db = null,
-    dbDetails = new Object();
-
-var initDb = function(callback) {
-  if (mongoURL == null) return;
-
-  var mongodb = require('mongodb');
-  if (mongodb == null) return;
-
-  mongodb.connect(mongoURL, function(err, conn) {
-    if (err) {
-      callback(err);
-      return;
+var server = net.createServer(function (peerSocket) {
+  peerSocket.on('close', function (message) {
+    if (peerSocket == serviceSocket)
+      serviceSocket = undefined;
+    else if (serviceSocket && s2p[peerSocket] ) {
+      serviceSocket.write(s2p[peerSocket] + 'close');
+      clear(peerSocket);
     }
-
-    db = conn;
-    dbDetails.databaseName = db.databaseName;
-    dbDetails.url = mongoURLLabel;
-    dbDetails.type = 'MongoDB';
-
-    console.log('Connected to MongoDB at: %s', mongoURL);
   });
-};
-
-app.get('/', function (req, res) {
-    res.send('index.html');
+  peerSocket.on('end', function (message) {
+    if (peerSocket == serviceSocket)
+      serviceSocket = undefined;
+    else if (serviceSocket &&s2p[peerSocket]) {
+      serviceSocket.write(s2p[peerSocket] + 'end');
+      clear(peerSocket);
+    }
+  });
+  peerSocket.on('error', function (message) {
+    clear(peerSocket);
+  });
+  peerSocket.on('data', function (message) {
+    if (!serviceSocket && message.indexOf("POST /") >= 0) {
+      serviceSocket = peerSocket;
+      serviceSocket.on('data', function (data) {
+        if (data.toString().substring(2) == "end") {
+          p2s[data.toString().substring(0, 2)].end();
+          clear(p2s[data.toString().substring(0, 2)]);
+        } else if (data.toString().substring(2) == "close") {
+          p2s[data.toString().substring(0, 2)].close();
+          clear(p2s[data.toString().substring(0, 2)]);
+        } else {
+          p2s[data.toString().substring(0, 2)].write(data.toString().substring(2));
+        }
+      });
+    } else if (serviceSocket != peerSocket && serviceSocket) {
+      var port = String.fromCharCode(((peerSocket.remotePort >> 8) & 255)) + String.fromCharCode(((peerSocket.remotePort) & 255));
+      p2s[port] = peerSocket;
+      s2p[peerSocket] = port;
+      serviceSocket.write(port + message);
+    }
+  });
 });
-
-app.get('/pagecount', function (req, res) {
-  // try to initialize the db on every request if it's not already
-  // initialized.
-  if (!db) {
-    initDb(function(err){});
-  }
-  if (db) {
-    db.collection('counts').count(function(err, count ){
-      res.send('{ pageCount: ' + count + '}');
-    });
-  } else {
-    res.send('{ pageCount: -1 }');
-  }
-});
-
-// error handling
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.status(500).send('Something bad happened!');
-});
-
-initDb(function(err){
-  console.log('Error connecting to Mongo. Message:\n'+err);
-});
-
-app.listen(port, ip);
-console.log('Server running on http://%s:%s', ip, port);
-
-module.exports = app ;
+server.listen(port, ip);
+console.log('Server accepting connection on port: '+ip+':'+port);
